3b4c94a8c95039c4bcfd1a33d1405711a6f4047d
Marius R <marius.rekus@gmail.com>
2022-11-28 11:55:11 +0200

    Nuskaityti class labels į listą

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import matplotlib.pyplot as plt
 config_file = "model_config/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
 frozen_model = "ssd_mobilenet_v3_large_coco_2020_01_14/frozen_inference_graph.pb"
 model = cv2.dnn_DetectionModel(frozen_model, config_file)
+
+classLabels = []
+label_file = "Labels.txt"
+with open(label_file, "r") as f:
+    classLabels.append(f.read().rstrip("\n").split("\n"))
+    classLabels = classLabels[0]
